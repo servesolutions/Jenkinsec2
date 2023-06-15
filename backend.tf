6f7f50f75464e92787dd7f261692184bbc94c2ae
Charles <info@servesolutions.io>
2023-06-15 22:17:14 +0100

    adding more sc for the project

---
+++ backend.tf
@@ -0,0 +1,10 @@
+## backend configuration
+
+terraform {
+    backend "s3" {
+        bucket = "jenkinsec2"
+        key = "terraform.tfstate"
+        region = "eu-west-2"
+        dynamodb_table = "jenkins"
+    }
+}
